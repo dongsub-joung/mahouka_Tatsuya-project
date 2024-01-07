e3ca0edf58866b95cd3aa5e081cd0d2f6c5cff3d
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-07 23:25:00 +0900

    get resource

--- stressTest/ddos/src/http_flood/mod.rs
+++ stressTest/ddos/src/http_flood/mod.rs
@@ -1,23 +1,42 @@
+use reqwest;
+
 // 1. post request in the form
 // 2. get resource(html,css,js)
-pub struct HttpFlood{
+pub struct HttpFlood {
     target_url: String,
-    times: u32
+    times: u32,
 }
 
 impl HttpFlood {
-    pub fn new(target_url: String, times: u32) -> Self{
-        Self{
-            target_url, times
+    pub fn new(target_url: String, times: u32) -> Self {
+        Self { target_url, times }
+    }
+
+    pub fn post_requeste_attack(&self, text: String) {}
+
+    pub async fn get_resource_attack(&self) {
+        // Create a reqwest Client
+        let client = reqwest::blocking::Client::new();
+
+        // Make a GET request to a URL
+        let response = client
+            .get(&self.target_url)
+            .send();
+
+        // Check if the request was successful
+        match response {
+            Ok(res) => {
+                if res.status().is_success() {
+                    // Read the response body as a string
+                    let body = res.text().unwrap();
+                    println!("Response body: {}", body);
+                } else {
+                    println!("Request failed with status code: {}", res.status());
+                }
+            }
+            Err(err) => {
+                println!("Error occurred: {}", err);
+            }
         }
     }
-
-    pub fn post_requeste_attack(&self, text: String){
-
-    }
-
-    pub fn get_resource_attack(&self){
-
-    }
 }
-
--- stressTest/ddos/src/main.rs
+++ stressTest/ddos/src/main.rs
@@ -1,15 +1,22 @@
+use std::error::Error;
+
 mod http_flood;
 mod utils;
 
-fn main(){
+#[tokio::main]
+async fn main() ->  Result<(),  Box<dyn Error>>{
     utils::show_logo();
+    
     let site= "example.com";
     let url= format!("http://{site}");
     let times= 2;
     let http_flood= http_flood::HttpFlood::new(url, times);
 
-    http_flood.get_resource_attack();
+    // Option 1
+    http_flood.get_resource_attack().await;
     
-    // limit 4k byte
-    // http_flood.post_requeste_attack(utils::get_empty_text_data());
+    // Option 2
+    http_flood.post_requeste_attack(utils::get_empty_text_data());
+
+    Ok(())
 }
