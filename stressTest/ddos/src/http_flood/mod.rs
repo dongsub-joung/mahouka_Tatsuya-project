301e8a6044a926e5021cdebf93fe4e4d540b1e14
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-01-10 09:27:23 +0900

    update

--- stressTest/ddos/src/http_flood/mod.rs
+++ stressTest/ddos/src/http_flood/mod.rs
@@ -12,31 +12,39 @@ impl HttpFlood {
         Self { target_url, times }
     }
 
-    pub fn post_requeste_attack(&self, text: String) {}
+    pub async fn post_requeste_attack(&self, text: String) {
+        for _ in 0..self.times{
+            
 
-    pub async fn get_resource_attack(&self) {
-        // Create a reqwest Client
-        let client = reqwest::blocking::Client::new();
-
-        // Make a GET request to a URL
-        let response = client
-            .get(&self.target_url)
-            .send();
-
-        // Check if the request was successful
-        match response {
-            Ok(res) => {
-                if res.status().is_success() {
-                    // Read the response body as a string
-                    let body = res.text().unwrap();
-                    println!("Response body: {}", body);
-                } else {
-                    println!("Request failed with status code: {}", res.status());
-                }
-            }
-            Err(err) => {
-                println!("Error occurred: {}", err);
-            }
         }
     }
+
+    pub async fn get_resource_attack(&self) {
+
+        for _ in 0..self.times{
+            // Create a reqwest Client
+            let client = reqwest::blocking::Client::new();
+    
+            // Make a GET request to a URL
+            let response = client
+                .get(&self.target_url)
+                .send();
+    
+            // Check if the request was successful
+            match response {
+                Ok(res) => {
+                    if res.status().is_success() {
+                        // Read the response body as a string
+                        let body = res.text().unwrap();
+                        println!("Response body: {}", body);
+                    } else {
+                        println!("Request failed with status code: {}", res.status());
+                    }
+                }
+                Err(err) => {
+                    println!("Error occurred: {}", err);
+                }
+            }
+        }
+        }
 }
--- stressTest/ddos/src/main.rs
+++ stressTest/ddos/src/main.rs
@@ -8,7 +8,7 @@ async fn main() ->  Result<(),  Box<dyn Error>>{
     utils::show_logo();
     
     let site= "example.com";
-    let url= format!("http://{site}");
+    let url: String= format!("http://{site}");
     let times= 2;
     let http_flood= http_flood::HttpFlood::new(url, times);
 
@@ -16,7 +16,7 @@ async fn main() ->  Result<(),  Box<dyn Error>>{
     http_flood.get_resource_attack().await;
     
     // Option 2
-    http_flood.post_requeste_attack(utils::get_empty_text_data());
+    http_flood.post_requeste_attack(utils::get_text_data());
 
     Ok(())
 }
