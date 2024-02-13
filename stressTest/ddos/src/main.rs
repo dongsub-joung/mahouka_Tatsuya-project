eca0c8ee878215461003b3073f69011453487f9b
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2024-02-13 16:28:57 +0900

    doing) match

--- stressTest/ddos/src/main.rs
+++ stressTest/ddos/src/main.rs
@@ -13,11 +13,13 @@ async fn main() ->  Result<(),  Box<dyn Error>>{
     let times= 2;
     let http_flood= http_flood::HttpFlood::new(url, times);
 
-    // Option 1
-    http_flood.get_resource_attack().await;
-    
-    // Option 2
-    // let _= http_flood.post_requeste_attack("body".to_string(), utils::get_text_data()).await;
+    let input_number=0_u16;
 
+    match input_number {
+        1 => http_flood.get_resource_attack().await,
+        2 => http_flood.post_requeste_attack("body".to_string(), utils::get_text_data()).await,
+        _ => println!("Unvalid code"),
+    }
+     
     Ok(())
 }
