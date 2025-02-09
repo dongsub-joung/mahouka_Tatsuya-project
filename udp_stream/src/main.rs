d8ad0e66c469e412c8dd75dc24eac53f8784b6db
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-02-09 19:31:24 +0900

    psedo

--- udp_stream/src/main.rs
+++ udp_stream/src/main.rs
@@ -1,10 +1,22 @@
-use std::net::UdpSocket;
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 fn main() {
     #[cfg(not(any(target_os = "windows")))]
     {
-        println!("Running on an unwindows operating system!");
+        println!("Running on an un-windows operating system!");
 
+        // about conn info
+        let localhost_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+        let local_port= 5000;
+        let remote_v4 = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+        let remote_port= 5000;
+
+        // connection
+
+
+        // check keeping conn
+        // mouse pointer x,y + screenshot copy
+        // shift + cnt + print-f(gnome)
     }
 
     #[cfg(target_os = "windows")]
