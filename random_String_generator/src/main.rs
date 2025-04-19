a0a8094ec515eacf86bab3521e5c48eb89d4cdeb
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-04-19 13:30:33 +0900

    random string generator

---
+++ random_String_generator/src/main.rs
@@ -0,0 +1,27 @@
+use rand::prelude::*;
+
+fn generate_random() -> u8 {
+    let mut rng = rand::rng();
+    // Generate and shuffle a sequence:
+    let mut nums: Vec<u8> = (33..126).collect();
+    nums.shuffle(&mut rng);
+    // And take a random pick (yes, we didn't need to shuffle first!):
+    
+    *(nums.choose(&mut rng).unwrap())
+}
+
+fn main(){
+    
+    let mut result= String::new();
+    let mut idx= 0;
+    while idx < 1000 {
+        let one_random_integer= generate_random();
+        let c= one_random_integer as char;
+
+        result.push_str(&format!("{}", c));
+        
+        idx+=1;
+    }
+
+    println!("{}", result);
+}
