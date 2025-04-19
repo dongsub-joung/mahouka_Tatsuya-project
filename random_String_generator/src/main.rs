0b828ed83fb5ad82c63172cc346b485c98e61e33
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-04-19 13:47:01 +0900

    fix

--- random_String_generator/src/main.rs
+++ random_String_generator/src/main.rs
@@ -1,13 +1,15 @@
 use rand::prelude::*;
 
-fn generate_random() -> u8 {
+fn generate_random() -> Option<u8> {
     let mut rng = rand::rng();
     // Generate and shuffle a sequence:
     let mut nums: Vec<u8> = (33..126).collect();
     nums.shuffle(&mut rng);
     // And take a random pick (yes, we didn't need to shuffle first!):
     
-    *(nums.choose(&mut rng).unwrap())
+    let result= *(nums.choose(&mut rng).unwrap());
+    
+    Some(result)
 }
 
 fn main(){
@@ -16,7 +18,7 @@ fn main(){
     let mut idx= 0;
     while idx < 1000 {
         let one_random_integer= generate_random();
-        let c= one_random_integer as char;
+        let c= one_random_integer.unwrap() as char;
 
         result.push_str(&format!("{}", c));
         
