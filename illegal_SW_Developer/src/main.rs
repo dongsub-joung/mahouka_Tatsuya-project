0fa536996c649799a4164402e59a1a8fb805b0e9
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-08-08 17:40:00 +0900

    Add clap

--- illegal_SW_Developer/src/main.rs
+++ illegal_SW_Developer/src/main.rs
@@ -2,10 +2,19 @@ mod spoofer_config;
 mod dhcp_server;
 mod dhcp_coerce;
 
-use std::env;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    iface: String,
+    domain_name: String,
+    target_server: String,
+    coere_ip: String,
+    relay_ip: String,
+}
 
 fn main() {
-    let key= env::args();
-    
-
+    let args = Args::parse();
 }
