d870ade4b028ef66b3109e991f2fc3492f9d9629
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-08-11 03:25:28 +0900

    as_dict fn 追加

--- illegal_SW_Developer/src/dhcp_server/mod.rs
+++ illegal_SW_Developer/src/dhcp_server/mod.rs
@@ -32,5 +32,19 @@ DNS Servers: {:?}
 Name Protection Enabled:  {}"
     , self.ip_address, self.domain_name, self.dns_servers, flag);
     }
+
+    pub fn as_dict(self) -> Self{
+        let name_protection_status= {if self.name_protection_status.is_none() {
+            String::from("Unknown")
+        }else{
+            String::from(self.name_protection_status.into())
+        }};
+        DHCPServer { 
+            ip_address: self.ip_address
+            , domain_name: self.domain_name
+            , dns_servers: self.dns_servers
+            , name_protection_status: self.name_protection_status 
+        }
+    }
 }
 
