dd6565f17f2d57755531c2aee394cdb602652d08
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-08-16 15:42:25 +0900

    Fix and Add) receive all packet

--- illegal_SW_Developer/DNS-Spoofing-Toolkit/v01/src/main.rs
+++ illegal_SW_Developer/DNS-Spoofing-Toolkit/v01/src/main.rs
@@ -1,16 +1,10 @@
-extern crate packet;
-
-use libc::*;
-use nfqueue::*;
-use std::fmt::Write;
-use packet::builder::Builder;
-use packet::icmp;
+use nfq::{Queue, Verdict};
 
 fn process_packet(packet: Vec<u8>){
     
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     const QUEUE_NUM: usize = 0;
 
     let (google_dns_host, google_ip)= 
@@ -21,24 +15,15 @@ fn main() {
     let dns_hosts: Vec<(Vec<u8>, &'static str)>= 
         Vec::from([(google_dns_host, google_ip), (facebook_dns_host, facebook_ip)]);
 
-    { // NetFilterQueue
-        let mut q = nfqueue::Queue::new();
-        let callback= process_packet();
-        
-        q.open();
-
-        let rc = q.bind(libc::AF_INET);
-        assert!(rc == QUEUE_NUM);
-
-        q.create_queue(QUEUE_NUM, callback);
-        q.set_mode(nfqueue::CopyMode::CopyPacket, 0xffff);
-
-        q.set_callback(callback);
-        q.run_loop();
-
-        q.close();
+    {
+        let mut queue = Queue::open()?;
+        queue.bind(0)?;
+        loop {
+            let mut msg = queue.recv()?;
+            msg.set_verdict(Verdict::Accept);
+            queue.verdict(msg)?;
+        }
     }
 
-
-    ipt.delete_chain("nat", "NEWCHAINNAME")
+    Ok(())
 }
