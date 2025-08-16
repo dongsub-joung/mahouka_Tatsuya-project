1336cb63ce2636376ac997102eb531dd7cdb3592
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-08-16 22:38:20 +0900

    troubleshooting) cannot return value referencing local data queue.packet

--- illegal_SW_Developer/DNS-Spoofing-Toolkit/v01/src/queue_overroding/mod.rs
+++ illegal_SW_Developer/DNS-Spoofing-Toolkit/v01/src/queue_overroding/mod.rs
@@ -1,16 +1,23 @@
 use crate::Queue;
+use std::collections::VecDeque;
+use nfq::Message;
+
 
 pub trait EnhanceQueue{
     fn new(packet: QueueOverride) -> Self;
-
 }
 
-pub struct QueueOverride{
-    payload: Queue::Payload,
+#[derive(Copy)]
+pub struct QueueOverride<'a>{
+    packet: Message,
+    payload: &'a [u8],
 }
 
-impl EnhanceQueue for QueueOverride{
-    fn new(packet: QueueOverride) -> Self {
-        QueueOverride { payload : packet.payload }
+impl EnhanceQueue for QueueOverride<'_>{
+    fn new(queue: QueueOverride) -> Self {
+        QueueOverride {
+            packet : queue.packet, 
+            payload : queue.packet.get_payload(),
+        }
     }
 }
