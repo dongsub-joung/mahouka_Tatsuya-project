b6bf79b10e08fd75fb3fc8d325e37f14416b4263
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-08-16 22:16:57 +0900

    ADD) blueprint impl EnhanceQueue

--- illegal_SW_Developer/DNS-Spoofing-Toolkit/v01/src/queue_overroding/mod.rs
+++ illegal_SW_Developer/DNS-Spoofing-Toolkit/v01/src/queue_overroding/mod.rs
@@ -1,2 +1,16 @@
 use crate::Queue;
 
+pub trait EnhanceQueue{
+    fn new(packet: QueueOverride) -> Self;
+
+}
+
+pub struct QueueOverride{
+    payload: Queue::Payload,
+}
+
+impl EnhanceQueue for QueueOverride{
+    fn new(packet: QueueOverride) -> Self {
+        QueueOverride { payload : packet.payload }
+    }
+}
