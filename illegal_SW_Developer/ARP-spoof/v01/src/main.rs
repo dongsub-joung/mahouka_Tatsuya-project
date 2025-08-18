e9c398a1ace009d906d3422d0da0cca11e3ed4d8
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-08-19 02:08:43 +0900

    枠組み

--- illegal_SW_Developer/ARP-spoof/v01/src/main.rs
+++ illegal_SW_Developer/ARP-spoof/v01/src/main.rs
@@ -1,6 +1,8 @@
 use std::{thread, time::Duration};
 
 use mac_address;
+use ipconfig;
+
 fn spoof(){
 
 }
@@ -9,16 +11,31 @@ fn restore(){
 
 }
 
+fn find_ethnet(ip_addrs: Vec<?>){
+
+}
+
 fn main() {
     let mac_addr= mac_address::get_mac_address().unwrap_or_else(|e| None);
+
+    let ip_addrs= {
+        let mut v= Vec::new();
+
+        for adapter in ipconfig::get_adapters()? {
+            v.push(adapter.ip_addresses());
+        }
+        v
+    };
 
     if mac_addr.is_none() {
         println!("Unknown OS or Unable OS");
     }else{
-        loop {
-            // null is handled by is_none(Always true)
-            let mac_addr= mac_addr.unwrap();
+        let ethernet_ip= find_ethnet(ip_addrs);
+        
+        // null is handled by is_none(Always true)
+        let mac_addr= mac_addr.unwrap();
 
+        loop {    
             // @Todo conn
             // 1. host
             spoof();
