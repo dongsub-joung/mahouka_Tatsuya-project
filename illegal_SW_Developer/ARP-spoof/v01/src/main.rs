3142d9c4d2cd86ecd44a49d2f75657dafcffe2f7
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-08-19 01:41:19 +0900

    枠組み

--- illegal_SW_Developer/ARP-spoof/v01/src/main.rs
+++ illegal_SW_Developer/ARP-spoof/v01/src/main.rs
@@ -1,3 +1,34 @@
+use std::{thread, time::Duration};
+
+use mac_address;
+fn spoof(){
+
+}
+
+fn restore(){
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mac_addr= mac_address::get_mac_address().unwrap_or_else(|e| None);
+
+    if mac_addr.is_none() {
+        println!("Unknown OS or Unable OS");
+    }else{
+        loop {
+            // null is handled by is_none(Always true)
+            let mac_addr= mac_addr.unwrap();
+
+            // @Todo conn
+            // 1. host
+            spoof();
+            // 2. target
+            spoof();
+
+            thread::sleep(Duration::from_secs(1));
+        }
+
+        // restore conn
+        restore();
+    }
 }
