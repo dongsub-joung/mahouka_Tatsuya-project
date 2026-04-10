9a450bd237fc3b8bb031f67816887577b318004b
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-04-10 11:34:10 +0900

    with Google Gemini

--- about_rust_lang/new_rust_features/src/eg_trait/mod.rs
+++ about_rust_lang/new_rust_features/src/eg_trait/mod.rs
@@ -1,23 +1,24 @@
-use std::ops::Add;
 
 pub trait Example {
+    // "located in stack as inline" Google Gemini notice  
     const CONST_NO_DEFAULT: i32;
     const CONST_WITH_DEFAULT: i8 = 2;
     type TypeNoDefault;
-
+    
     fn method_with_default()-> i8{ 1 }
     fn method_without_default<T: Example>(&self) -> i8;
 }
 
-pub struct eg_struct{
-    for_typeNoDefault: i32,
+pub struct EgStruct{
+    for_type_no_default: i32,
 }
 
-impl Example for eg_struct {
+impl Example for EgStruct {
     type TypeNoDefault= i8;
     const CONST_NO_DEFAULT: i32 = 1;
-    const CONST_WITH_DEFAULT: i8 = 2;
-    
+    // const CONST_WITH_DEFAULT: i8 = 2;  not required/optional
+
+
     fn method_with_default()-> i8{
         1
     }
--- about_rust_lang/new_rust_features/src/main.rs
+++ about_rust_lang/new_rust_features/src/main.rs
@@ -5,10 +5,13 @@ use crate::eg_trait::*;
 use crate::eg_generictraits::*;
 
 
+static A: i32 =1; // "located in Rust's Data Segment" Google Gemini notice
+
 fn get_one<T:eg_trait::Example>()-> i32{
     T::CONST_NO_DEFAULT
 }
 
 fn main(){
-    let status_one= get_one::<eg_struct>();
+    // For Explicit Declaration, Its same thing parse::<usize>()
+    let status_one= get_one::<eg_trait::EgStruct>();
 }
