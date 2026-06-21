78f58fe2a22dad0a861aa52f6fd12a4fe7ddefa9
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-21 15:07:38 +0900

    Add) keyboard_handler

--- ruby_script/keyboad_logger.rb
+++ ruby_script/keyboad_logger.rb
@@ -1,6 +1,26 @@
-class KeyboadObj
+class KeyboardObj
+  attr_accessor input_way
+
+  def initialize()
+    # or Os.io.keyboard
+    @input_way= Os.io.Keyboard_data_bus 
+  end
+end
+
+class KeyboardHandler
   attr_accessor a_key
+  
   def initialize(one_keyboad_obj)
     @a_key= one_keyboad_obj
+  end
+end
+
+# itit
+loop
+  # new set keyboard object up 
+  keyboard_obj= KeyboardObj.new()
+  # handle that input
+  keyboard_handler= KeyboardHandler.new(keyboard_obj)
   
-keyboard_obj= KeyboadObj.new()
+  
+end
