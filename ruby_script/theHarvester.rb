479277942f1cf10e7173907926f8775dfa29fdd5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-22 15:00:38 +0900

    Add) Data

--- ruby_script/theHarvester.rb
+++ ruby_script/theHarvester.rb
@@ -1,3 +1,13 @@
+class Data
+  def initialize()
+    return self
+  end
+  
+  def set_data(gethered_data)
+    # @TODO migration bot's data -> Data obj
+  end
+end
+
 class Havester
     attr_accessor nickname
     attr_accessor url
@@ -9,7 +19,21 @@ class Havester
     def set_targer_url(url)
       @url= url
     end
+    
+    def crowing_bot()
+      bot= bot.new(@nickname, @url)
+      
+      begin
+        bot.init
+      rescue CrowingErr
+        "failed bot init"
+      ensure
+        return Data.new()
+      end
 
+      gethered_data= bot.get_data()
+      return Data.set_data(gethered_data)
+    end
 end
 
 nickname= 'dongsub'
@@ -20,3 +44,6 @@ puts h.user_nickname
 
 h.set_targer_url(url)
 puts h.url
+
+data= h.crowing_bot()
+puts data
